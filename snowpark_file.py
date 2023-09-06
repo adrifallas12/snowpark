21c9a6f8ff64711d6480bc73dcd9bb1de328d239
adrifallas12 <143028706+adrifallas12@users.noreply.github.com>
2023-09-06 10:45:02 -0600

    Update snowpark_file.py
    added the syntax for the connection with snowflake

--- snowpark_file.py
+++ snowpark_file.py
@@ -1,1 +1,23 @@
+from snowflake.snowpark.session import Session
+import pandas as pd
 
+# import sf credentials
+import configparser
+config_file = 'sf_creds.config'
+config = configparser.ConfigParser()
+config.read(config_file)
+
+
+connection_parameters = {
+    "account": config['DEFAULT']['SQCOROF-MX53774'],
+    "user": config['DEFAULT']['Adriana Fallas'],
+    "password": config['DEFAULT']['TexasTech2023'],
+    "role": config['DEFAULT']['data_science'],
+    "warehouse": config['DEFAULT']['ds_warehouse'],
+    "database": config['DEFAULT']['snowpark_capstone_db'],
+    "schema": config['DEFAULT']['adriana_fallas_schema']
+ }
+
+# Create and Verify Session
+session = Session.builder.configs(connection_parameters).create()
+session.add_packages("snowflake-snowpark-python", "pandas")
